762ecb21122ef8a8c147f938d1f12e4ffc6f01b4
erpachecomo <epachecomorelos@gmail.com>
2025-06-03 12:15:43 -0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
+import { defineConfig } from "astro/config";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+});
---
+++ src/components/Stats.tsx
@@ -0,0 +1,32 @@
+import type React from "react";
+import { Stat } from "poke-ui-react";
+
+const skills = [
+  { label: "TypeScript", value: 85 }, 
+  { label: "Python", value: 85 }, 
+  { label: "Golang", value: 75 }, 
+  { label: "React", value: 95 }, 
+  { label: "Node.js", value: 85 }, 
+  { label: "Jest/RTL/Cypress", value: 80 }, 
+  { label: "AWS", value: 85 }, 
+]
+
+const Stats: React.FC = () => (
+  <div className="flex-grow justify-between">
+    <h2 className="text-2xl font-semibold mt-4 text-primary-dark">Stats</h2>
+    <div className="flex flex-col justify-evenly gap-y-5">
+      {skills.map((value) => (
+        <Stat
+          label={value.label}
+          value={value.value}
+          classNames={{
+            labelsContainer: "justify-between",
+            barFill: "scale-in-hor-left bg-primary",
+            bar: "bg-accent",
+          }}
+        />
+      ))}
+    </div>
+  </div>
+);
+export default Stats;
