ad95b47d903e5960cff5545bd901ef3021c9780e
erpachecomo <epachecomorelos@gmail.com>
2025-06-05 18:14:36 -0700

    fix: wording on about and style improvements

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -2,27 +2,39 @@ import type React from "react";
 import { Stat } from "poke-ui-react";
 
 const skills = [
-  { label: "TypeScript", value: 85 }, 
-  { label: "Python", value: 85 }, 
-  { label: "Golang", value: 75 }, 
-  { label: "React", value: 95 }, 
-  { label: "Node.js", value: 85 }, 
-  { label: "Jest/RTL/Cypress", value: 80 }, 
-  { label: "AWS", value: 85 }, 
+  { label: "TypeScript", value: 7 }, 
+  { label: "Python", value: 4 }, 
+  { label: "Golang", value: 1 }, 
+  { label: "React", value: 7 }, 
+  { label: "Node.js", value: 5 }, 
+  { label: "Jest/RTL/Cypress", value: 6 }, 
+  { label: "AWS", value: 4 }, 
 ]
 
 const Stats: React.FC = () => (
   <div className="flex-grow justify-between">
-    <h2 className="text-2xl font-semibold mt-4 text-primary-dark">Stats</h2>
+    <h2 className="text-2xl font-semibold mt-4 text-primary-dark">Main Tech Stack</h2>
     <div className="flex flex-col justify-evenly gap-y-5">
+      <Stat
+          label={"Technology"}
+          value={"Years of experience"}
+          maxValue={7}
+          minValue={0}
+          classNames={{
+            labelsContainer: "justify-between text-gray-800",
+            barFill: "bg-accent h-2",
+          }}
+        />
       {skills.map((value) => (
         <Stat
           label={value.label}
           value={value.value}
+          maxValue={7}
+          minValue={0}
           classNames={{
             labelsContainer: "justify-between text-gray-800",
-            barFill: "scale-in-hor-left bg-primary",
-            bar: "bg-accent",
+            barFill: "scale-in-hor-left bg-primary h-2",
+            bar: "bg-gray-300 h-2",
           }}
         />
       ))}
